20f1e8987c22be60d262119409c42fac99236d14
dario <darioyzf@gmail.com>
2016-01-25 21:42:40 +0100

    tobetryed

--- bin/ddclone.ts
+++ bin/ddclone.ts
@@ -1,19 +1,14 @@
 import clone = require("../index");
 
-
 let source = process.argv[2];
 let dest = process.argv[3];
 
-
-if (source) {
     clone(source, dest).then(function() {
         console.log("ok");
     }).catch(function(err) {
-        throw Error(err);
+        console.log("ERROR:",err);
     });
-} else {
-    throw Error("missing source");
-}
+
 
 
 
--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import * as Promise from "bluebird";
+import * as pathExists from "path-exists";
 import * as child_process from "child_process";
 
 let exec = child_process.exec;
@@ -16,8 +17,6 @@ function shacheck(path: string, bs?: string, count?: string) {
                     reject(stderr);
 
                 } else {
-
-
                     resolve(stdout.toString("utf-8"));
                 }
             });
@@ -34,80 +33,179 @@ function shacheck(path: string, bs?: string, count?: string) {
                 }
             });
         }
-
-
-
-
-
     });
 }
 
-export =function(source: string, dest: string) {
-    return new Promise<boolean>(function(resolve, reject) {
+function filesize(file: string) {
+    return new Promise<number>(function(resolve, reject) {
+        exec("ls -s " + file, function(err, stdout, stderr) {
+            if (err) {
+                reject(err);
+            } else if (stderr) {
+                reject(stderr);
 
-        let disk: any = false;
+            } else {
+                resolve(parseInt(stdout.toString("utf-8")));
+            }
+        });
+    });
+}
 
-        if (source.split("dev/").length == 2) {
+function disksize(disk: string) {
+    return new Promise<number>(function(resolve, reject) {
+        exec("echo $(( $(sudo fdisk " + disk + " -l | grep " + disk + ":| awk {'print($5)'}) / 1024 ))", function(err, stdout, stderr) {
+            if (err) {
+                reject(err);
+            } else if (stderr) {
+                reject(stderr);
 
-            disk = source;
+            } else {
+                resolve(parseInt(stdout.toString("utf-8")));
+            }
+        });
+    });
+}
 
-        } else if (dest.split("dev/").length == 2) {
-            disk = dest;
-        }
+function diskbusysize(disk: string) {
+    return new Promise<number>(function(resolve, reject) {
+        exec("fdisk " + disk + "' -l", function(err, stdout, stderr) { // get disk source size taking the last block of last partition
+            if (err) {
+                reject(err);
+            } else if (stderr) {
+                reject(stderr);
 
-        if (disk) {
+            } else {
 
-            exec("fdisk -l | grep '" + disk + "' -A 2 ", function(err, stdout, stdin) {
 
                 let fdiskstring = stdout.toString("utf-8");
                 let fdisklines = fdiskstring.split("\n");
-                let bs = fdisklines[2].replace(/ +(?= )/g, "").split(" ")[0];
-                let count = fdisklines[2].replace(/ +(?= )/g, "").split(" ")[0];
+                let bs = parseInt(fdisklines[2].replace(/ +(?= )/g, "").split(" ")[3]);
+                let count = parseInt(fdisklines[fdisklines.length - 1].replace(/ +(?= )/g, "").split(" ")[3]);
 
-                let cmd = "dd if=" + source + " bs=" + bs + " count=" + count + " of=" + dest;
+                resolve((bs * count) / 1024);
 
-                console.log(cmd);
+            }
+        });
+    });
+}
 
-                shacheck(source, bs, count).then(function(sha1) {
+
+function freespace(file: string) {
+    return new Promise<number>(function(resolve, reject) {
+        exec("df -k " + file.split("/")[file.split("/").length - 1], function(err, stdout, stderr) {
+            if (err) {
+                reject(err);
+            } else if (stderr) {
+                reject(stderr);
+
+            } else {
+                resolve(parseInt(stdout.toString("utf-8")));
+            }
+        });
+    });
+}
 
 
-                    exec(cmd, function(err, stdout, stderr) {
-                        if (err) {
-                            reject(err);
-                        } else if (stderr) {
-                            reject(stderr);
+function checkspace(source: string, dest: string) {
+    return new Promise<boolean>(function(resolve, reject) {
+        if (source.split("dev/").length == 2) {
+            diskbusysize(source).then(function(sourcesize) {
 
+                if (dest.split("dev/").length == 2) {
+
+                    disksize(dest).then(function(sizedest) {
+
+                        if (sourcesize < sizedest) {
+                            resolve(true);
                         } else {
-
-
-                            shacheck(dest, bs, count).then(function(sha2) {
-                                if (sha1 == sha2) {
-                                    resolve(true);
-                                } else {
-                                    reject("shasum don't match");
-                                }
-
-                            }).catch(function(err) {
-                                reject(err);
-                            });
+                            reject("insufficient space on " + dest);
                         }
+
+                    }).catch(function(err) {
+                        reject(err);
                     });
-                }).catch(function(err) {
-                    reject(err);
-                });
+
+
+                } else {
+
+                    freespace(dest).then(function(sizedest) {
+
+
+
+                        if (sourcesize < sizedest) {
+                            resolve(true);
+                        } else {
+                            reject("insufficient space on " + dest);
+                        }
+                    }).catch(function(err) {
+                        reject(err);
+                    });
+                }
+
+
+            }).catch(function(err) {
+                reject(err);
             });
 
+
         } else {
+            filesize(source).then(function(sizesource) {
+
+                if (dest.split("dev/").length == 2) {
+                    disksize(dest).then(function(sizedest) {
+                        if (sizesource < sizedest) {
+                            resolve(true);
+                        } else {
+                            reject("insufficient space on " + dest);
+                        }
+                    }).catch(function(err) {
+                        reject(err);
+                    });
 
 
-            let cmd = "dd if=" + source + " of=" + dest;
+                } else {
 
-            console.log(cmd);
+                    freespace(dest).then(function(sizedest) {
 
-            shacheck(source).then(function(sha1) {
+                        if (sizesource < sizedest) {
+                            resolve(true);
+                        } else {
+                            reject("insufficient space on " + dest);
+                        }
 
-                exec(cmd, function(err, stdout, stderr) {
+                    }).catch(function(err) {
+                        reject(err);
+                    });
 
+                }
+
+
+            }).catch(function(err) {
+                reject(err);
+            });
+
+
+        }
+    });
+}
+
+
+function umount_drive(disk) {
+    return new Promise<boolean>(function(resolve, reject) {
+        exec("cat /proc/mounts | grep " + disk, function(err, stdout, stderr) {
+            if (err) {
+                reject(err);
+            } else if (stderr) {
+                reject(stderr);
+
+            } else {
+                let drives = "";
+                let fdiskstring = stdout.toString("utf-8");
+                var fdisklines = fdiskstring.split("\n");
+                for (var i = 0; i < fdisklines.length; i++) {
+                    drives = drives + fdisklines[i] + " ";
+                }
+                exec("umount " + drives, function(err, stdout, stderr) {
                     if (err) {
                         reject(err);
                     } else if (stderr) {
@@ -115,24 +213,172 @@ export =function(source: string, dest: string) {
 
                     } else {
 
+                        resolve(true);
+                    }
+                });
+            }
+        });
+    });
+}
 
-                        shacheck(dest).then(function(sha2) {
-                            if (sha1 == sha2) {
-                                resolve(true);
+
+function umountall(source: string, dest: string) {
+    return new Promise<boolean>(function(resolve, reject) {
+        if (source.split("dev/").length == 2) {
+            umount_drive(source).then(function() {
+                if (dest.split("dev/").length == 2) {
+                    umount_drive(dest).then(function() {
+                        resolve(true);
+                    }).catch(function(err) {
+                        reject(err);
+                    });
+                } else {
+                    resolve(true);
+                }
+            }).catch(function(err) {
+                reject(err);
+            });
+        } else {
+            if (dest.split("dev/").length == 2) {
+                umount_drive(dest).then(function() {
+                    resolve(true);
+                }).catch(function(err) {
+                    reject(err);
+                });
+            } else {
+                resolve(true);
+            }
+
+        }
+
+
+
+    });
+}
+
+
+export =function(source: string, dest: string, progress?: Function) {
+    return new Promise<boolean>(function(resolve, reject) {
+        if (!source || !pathExists.sync(source)) {
+            reject("missing source");
+        } else if (!dest) {
+            reject("missing dest");
+        } else {
+
+            let disk: any = false;
+
+            if (source.split("dev/").length == 2) {
+
+                disk = source;
+
+            } else if (dest.split("dev/").length == 2) {
+                disk = dest;
+                if (!pathExists.sync(dest)) {
+                    reject("missing dest");
+                }
+
+            }
+            umountall(source, dest).then(function() {
+                checkspace(source, dest).then(function() {
+
+
+                    if (disk) {
+
+
+                        exec("fdisk " + disk + " -l", function(err, stdout, stderr) {
+
+
+                            if (err) {
+                                reject(err);
+                            } else if (stderr) {
+                                reject(stderr);
+
                             } else {
-                                reject("shasum don't match");
-                            }
 
+
+
+                                let fdiskstring = stdout.toString("utf-8");
+                                let fdisklines = fdiskstring.split("\n");
+                                let bs = fdisklines[2].replace(/ +(?= )/g, "").split(" ")[3];
+                                let count = fdisklines[fdisklines.length - 1].replace(/ +(?= )/g, "").split(" ")[3];
+
+                                let cmd = "dd if=" + source + " bs=" + bs + " count=" + count + " of=" + dest;
+
+                                console.log(cmd);
+
+                                shacheck(source, bs, count).then(function(sha1) {
+                                    exec(cmd, function(err, stdout, stderr) {
+                                        if (err) {
+                                            reject(err);
+                                        } else if (stderr) {
+                                            reject(stderr);
+
+                                        } else {
+
+
+                                            shacheck(dest, bs, count).then(function(sha2) {
+                                                if (sha1 == sha2) {
+                                                    resolve(true);
+                                                } else {
+                                                    reject("shasum don't match");
+                                                }
+
+                                            }).catch(function(err) {
+                                                reject(err);
+                                            });
+                                        }
+                                    });
+                                }).catch(function(err) {
+                                    reject(err);
+                                });
+                            }
+                        });
+
+                    } else {
+
+
+                        let cmd = "dd if=" + source + " of=" + dest;
+
+                        console.log(cmd);
+
+                        shacheck(source).then(function(sha1) {
+
+                            exec(cmd, function(err, stdout, stderr) {
+
+                                if (err) {
+                                    reject(err);
+                                } else if (stderr) {
+                                    reject(stderr);
+
+                                } else {
+
+
+                                    shacheck(dest).then(function(sha2) {
+                                        if (sha1 == sha2) {
+                                            resolve(true);
+                                        } else {
+                                            reject("shasum don't match");
+                                        }
+
+                                    }).catch(function(err) {
+                                        reject(err);
+                                    });
+                                }
+                            });
                         }).catch(function(err) {
                             reject(err);
                         });
                     }
+
+                }).catch(function(err) {
+                    reject(err);
                 });
             }).catch(function(err) {
                 reject(err);
             });
-        }
 
+
+        }
     });
 
 
